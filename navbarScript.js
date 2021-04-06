e9deb63f1049a6655106bfbde939d80ba34b50a6
RobbertChampagne <69464893+RobbertChampagne@users.noreply.github.com>
2021-04-06 16:08:53 +0200

    Add files via upload

---
+++ navbarScript.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", loaded);
+
+function loaded(){
+
+    if(window.innerWidth < 873){
+        
+        document.getElementById("hamburgerMenuA").addEventListener("click", showOrHideDropdownMenu);
+    
+        function showOrHideDropdownMenu(){
+            let navbar = document.getElementById("navbar");
+            let usedClass = navbar.getAttribute("class");
+            
+            if(usedClass === "navbarHide"){
+                navbar.setAttribute("class", "navbarShow");
+            }else{
+                navbar.setAttribute("class", "navbarHide");
+            }
+        }
+
+        
+    }else{
+
+    }
+
+    
+}
